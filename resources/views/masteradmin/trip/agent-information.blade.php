c7b905ebc167b674f7ab898b06e020ea767038f8
tejpal-navadiya <149236405+tejpal-navadiya@users.noreply.github.com>
2024-10-08 10:52:05 +0530

    view trip - agent details show

---
+++ resources/views/masteradmin/trip/agent-information.blade.php
@@ -0,0 +1,13 @@
+<div class="card">
+    <div class="col-lg-12 card-body3">
+        <div class="row align-items-center">
+            <div class="col-lg-6 col-md-6">
+                <p class="company_business_name">Name :{{ $trip->tr_agent_id ?? ''}}</p>
+                <p class="company_business_name">Phone Number : {{ $trip->tr_phone ?? '' }}</p>
+                <p class="company_business_name">Email Address : {{ $trip->tr_email ?? ''}}</p>
+                <p class="company_business_name">Address : {{ $trip->tr_phone ?? '' }}</p>
+            </div>
+        </div>
+    </div>
+
+</div>
